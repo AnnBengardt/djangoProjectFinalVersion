01fd90210ca953dfc296a06a448eb5534e8eb444
daniiiL97 <151668018+daniiiL97@users.noreply.github.com>
2023-12-02 02:01:33 +0300

    task3

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-
-class Post(models.Model):
+class Post (models.Model):
     title = models.CharField(primary_key=True, max_length=1000)
     body = models.TextField()
+    image = models.ImageField(blank=True, upload_to="images")
 
     def __str__(self):
         return self.title
